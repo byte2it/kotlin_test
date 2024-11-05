68a59062f10f04ff56f38eb07e9617d1e8f61443
Denys Ponomarenko <dolphinden007@gmail.com>
2024-11-04 22:36:21 -0500

    Create HttpStatusCode.java

---
+++ java_server/HttpStatusCode.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Map;
+
+public class HttpStatusCode {
+    public static final Map<Integer, String> STATUS_CODES = Map.of(200, "OK", 400, "BAD_REQUEST", 404, "NOT_FOUND", 500, "INTERNAL_SERVER_ERROR");
+
+    public HttpStatusCode() {
+    }
+}
